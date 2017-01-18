9c6a631b16eb570157ccdb9cb84b52aae6d8404e
Herminio García González <herminiogg@hotmail.com>
2017-01-18 17:26:17 +0100

    Added support for all XSDTypes

--- src/main/scala-2.11/es/weso/xmlschema2shex/parser/Parser.scala
+++ src/main/scala-2.11/es/weso/xmlschema2shex/parser/Parser.scala
@@ -11,56 +11,64 @@ import es.weso.xmlschema2shex.generation.CodeGenerator
   */
 object Parser extends XMLSchemaParser{
   def main(args: Array[String]): Unit = {
-    val xml = """<?xml version="1.0" encoding="UTF-8" ?>
+    val xml = """<?xml version="1.0"?>
                 |<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-                |
-                |<xs:simpleType name="stringtype">
-                |  <xs:restriction base="xs:string"/>
-                |</xs:simpleType>
-                |
-                |<xs:simpleType name="inttype">
-                |  <xs:restriction base="xs:positiveInteger"/>
-                |</xs:simpleType>
-                |
-                |<xs:simpleType name="dectype">
-                |  <xs:restriction base="xs:decimal"/>
-                |</xs:simpleType>
-                |
-                |<xs:simpleType name="orderidtype">
-                |  <xs:restriction base="xs:string">
-                |    <xs:pattern value="[0-9]{6}"/>
-                |  </xs:restriction>
-                |</xs:simpleType>
-                |
-                |<xs:complexType name="shiptotype">
-                |  <xs:sequence>
-                |    <xs:element name="name" type="stringtype"/>
-                |    <xs:element name="address" type="stringtype"/>
-                |    <xs:element name="city" type="stringtype"/>
-                |    <xs:element name="country" type="stringtype"/>
-                |  </xs:sequence>
+                |<xs:attribute name="id" type="xs:positiveInteger"/>
+                |<xs:complexType name="station">
+                |    <xs:sequence>
+                |        <xs:element name="name" type="xs:string" />
+                |    </xs:sequence>
+                |    <xs:attribute ref="id" use="required"/>
                 |</xs:complexType>
-                |
-                |<xs:complexType name="itemtype">
-                |  <xs:sequence>
-                |    <xs:element name="title" type="stringtype"/>
-                |    <xs:element name="note" type="stringtype" minOccurs="0"/>
-                |    <xs:element name="quantity" type="inttype"/>
-                |    <xs:element name="price" type="dectype"/>
-                |  </xs:sequence>
+                |<xs:complexType name="driver">
+                |    <xs:sequence>
+                |        <xs:element name="name" type="xs:string" />
+                |        <xs:element name="birth_date" type="xs:date" />
+                |        <xs:element name="email" type="xs:string" />
+                |        <xs:element name="phone" type="xs:string" />
+                |        <xs:element name="avatar" type="xs:string" />
+                |    </xs:sequence>
+                |    <xs:attribute ref="id" use="required"/>
                 |</xs:complexType>
-                |
-                |<xs:complexType name="shipordertype">
-                |  <xs:sequence>
-                |    <xs:element name="orderperson" type="stringtype"/>
-                |    <xs:element name="shipto" type="shiptotype"/>
-                |    <xs:element name="item" maxOccurs="unbounded" type="itemtype"/>
-                |  </xs:sequence>
-                |  <xs:attribute name="orderid" type="orderidtype" use="required"/>
-                |</xs:complexType>
-                |
-                |<xs:element name="shiporder" type="shipordertype"/>
-                |
+                |<xs:element name="lvb_system">
+                |    <xs:complexType>
+                |        <xs:sequence>
+                |            <xs:element name="line" minOccurs="0" maxOccurs="unbounded" >
+                |                <xs:complexType>
+                |                    <xs:sequence>
+                |                        <xs:element name="code" type="xs:string" />
+                |                        <xs:element name="type" type="xs:string" />
+                |                        <xs:element name="start_time_operation" type="xs:time" />
+                |                        <xs:element name="end_time_operation" type="xs:time" />
+                |                        <xs:element name="count_vehicles">
+                |                          <xs:simpleType>
+                |                            <xs:restriction base="xs:integer">
+                |                              <xs:minInclusive value="0"/>
+                |                              <xs:maxInclusive value="10"/>
+                |                            </xs:restriction>
+                |                          </xs:simpleType>
+                |                        </xs:element>
+                |                        <xs:element name="map" type="xs:string" />
+                |                        <xs:element name="start_station" type="station" minOccurs="0" maxOccurs="1" />
+                |                        <xs:element name="end_station" type="station" minOccurs="0" maxOccurs="1" />
+                |                        <xs:element name="intermediate_stations" type="station" minOccurs="0" maxOccurs="5" />
+                |                        <xs:element name="vehicles_line" minOccurs="0" maxOccurs="10" >
+                |                            <xs:complexType>
+                |                                <xs:sequence>
+                |                                    <xs:element name="name" type="xs:string" />
+                |                                    <xs:element name="capacity" type="xs:integer" />
+                |                                    <xs:element name="driver" minOccurs="0" maxOccurs="unbounded" type='driver' />
+                |                                </xs:sequence>
+                |                                <xs:attribute ref="id" use="required"/>
+                |                            </xs:complexType>
+                |                        </xs:element>
+                |                    </xs:sequence>
+                |                    <xs:attribute ref="id" use="required"/>
+                |                </xs:complexType>
+                |            </xs:element>
+                |        </xs:sequence>
+                |    </xs:complexType>
+                |</xs:element>
                 |</xs:schema>""".stripMargin
 
     val output = XMLSchema2ShexParser().parse(xml, None)
